9e993adade23ecdd017bc5c732848e747b5befb4
Michael Bekker <bekker.michael2@gmail.com>
2017-02-13 13:11:23 +0200

    Visits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123134647) do
+ActiveRecord::Schema.define(version: 20170121135543) do
 
   create_table "channels", force: :cascade do |t|
     t.string "name"
@@ -57,16 +57,6 @@ ActiveRecord::Schema.define(version: 20170123134647) do
     t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "visits", force: :cascade do |t|
-    t.boolean  "order"
-    t.boolean  "return"
-    t.decimal  "order_value"
-    t.text     "store_issue"
-    t.text     "feedback"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
 end
