6e6bebdee1ec4d1b93a1def29b8d1cb65c12a2cb
Michael Bekker <bekker.michael2@gmail.com>
2017-02-15 15:43:59 +0200

    Test

--- public/javascript/lib/gmaps.js
+++ public/javascript/lib/gmaps.js
@@ -1,23 +1,41 @@
-<script>
-      function initMap() {
-        <%- @stores.near([@latitude, @longitude], 0.5).each do |store| %>
-        var lat = <%= store.latitude %>
-        var lng = <%= store.longitude %>
-        var mylatlng = {lat: lat, lng: lng}
-        <% end %>
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 15,
-          center: mylatlng
-        });
-        
-        var marker = new google.maps.Marker({
-          position: mylatlng,
-          title: 'Click to Visit this store',
-          map: map
-        });
-    }
-     
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDsnTpV-sEjE_DZdX7IggTizvfn9b_eV3s&callback=initMap">
-    </script>
+<%- @locations.map {|store| "<div id='store_#{store.id}' class='store-object' data-latitude='#{store.latitude}' data-longitude='#{store.longitude}' data-name='#{store.storename}' data-path='#{store_path(store)}'></div>".html_safe} %>
+
+function initMap() {
+  var $stores = $(".store-object");
+  $stores.each(function() {
+    var lat = $(this).data("latitude");
+    var lng = $(this).data("longitude");
+    var mylatlng = {lat: lat, lng: lng}
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: mylatlng
+    }); 
+
+    var marker = new google.maps.Marker({
+      position: mylatlng,
+      map: map,
+      animation: google.maps.Animation.DROP,
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+      var c = confirm('Would you like to visit this store?')
+      if (c === true) {
+        window.location.href = $(this).data("path");  
+      }
+      if (c === false) {
+        window.location.href = '<%= dashboard_path %>';   
+      }
+    });
+
+    var contentString = 'Please click on this Marker to visit' + ' ' + $(this).data("name");   
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    google.maps.event.addListenerOnce(map, 'tilesloaded', function() {
+      infowindow.open(map, marker);
+    });  
+  });
+}
