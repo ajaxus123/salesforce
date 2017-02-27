faba76d00c486bda9da15a0187950e4e73a386a0
Michael Bekker <bekker.michael2@gmail.com>
2017-02-27 15:54:10 +0200

    gmap

--- public/javascript/lib/tracklocation.js
+++ public/javascript/lib/tracklocation.js
@@ -1,81 +1,94 @@
-var watchID;
-var geo; // for the geo object
-var map; // google map object
-var mapMarker; // map marker object
+        var watchID;
+        var geo;    // for the geolocation object
+        var map;    // for the google map object
+        var mapMarker;  // the google map marker object
 
-// position option
-var MAXIMUM_AGE = 200; // milliseconds
-var TIMEOUT = 300000;
-var HIGHACCURACY = true;
+        // position options
+        var MAXIMUM_AGE = 200; // miliseconds
+        var TIMEOUT = 300000;
+        var HIGHACCURACY = true;
 
-function getGeoLocation() {
-	try {
-		if( !! navigator.geolocation ) return navigator.geolocation;
-		else return undefined;		
-	} catch(e) {
-		return undefined;
-	}
-}
+        function getGeoLocation() {
+            try {
+                if( !! navigator.geolocation ) return navigator.geolocation;
+                else return undefined;
+            } catch(e) {
+                return undefined;
+            }
+        }
 
-function show_map(position) {
-	var lat = position.coords.latitude;
-	var lng = position.coords.longitude;
-	var latlng = new google.maps.LatLng(lat, lng);
+        function initMap(position) {
+            var lat = position.coords.latitude;
+            var lon = position.coords.longitude;
+            var latlng = new google.maps.LatLng(lat, lon);
 
-	if(map) {
-		map.panTo(latlng);
-		mapMarker.setPosition(latlng);
-	} else {
-		var myOptions = {
-			zoom: 13,
-			center: latlng,
-		};
-		
-		map = new google.maps.Map(document.getElementById("map"), myOptions);
-		map.setTilt(0); // turns of default 45% map tilt
+            if(map) {
+                map.panTo(latlng);
+                mapMarker.setPosition(latlng);
+            } else {
+                var myOptions = {
+                    zoom: 18,
+                    center: latlng,
 
-		mapMarker = new google.maps.Marker({
-			position: latlng,
-			title: "You are here."
-		});
-		mapMarker.setMap(map);
-	}
-}
+                    // mapTypeID --
+                    // ROADMAP displays the default road map view
+                    // SATELLITE displays Google Earth satellite images
+                    // HYBRID displays a mixture of normal and satellite views
+                    // TERRAIN displays a physical map based on terrain information.
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+                map = new google.maps.Map(document.getElementById("map"), myOptions);
+                map.setTilt(0); // turns off the annoying default 45-deg view
 
-function geo_error(error) {
-	stopWatching();
-	switch(error.code) {
-		case error.TIMEOUT:
-			alert('Geolocation Timed Out');
-			break;
-		case error.POSITION_UNAVAILABLE:
-			alert('Geolocation is unavailable');
-			break;
-		case error.PERMISSION_DENIED:
-			alert('Geolocation permission was denied.');
-			break;
-		default:
-			alert('Geolocation returned an unknown error code: '+ error.code);	
-	}
-}
+                mapMarker = new google.maps.Marker({
+                    position: latlng,
+                    title:"You are here."
+                });
+                mapMarker.setMap(map);
 
-function stopWatching() {
-	if(watchID) geo.clearwatch(watchID);
-	watchID = null;
-}
+                mapMarker.addListener('click', function(position) {
+                window.open("stores?latitude="+lat+"&longitude="+lon,"_self")
+                });
+            }
 
-function stopWatching() {
-	watchID = geo.watchPosition(show_map, geo_error, {
-		enableHighAccuracy: HIGHACCURACY,
-		maximumAge: MAXIMUM_AGE,
-		timeout: TIMEOUT
-	});
-}
 
-window.onload = function() {
-	if(geo = getGeoLocation()) {
-		stopWatching();
-	} else {
-		alert('Geolocation not supported by this browser')
-	}
-}
+        }
+
+        function geo_error(error) {
+            stopWatching();
+            switch(error.code) {
+                case error.TIMEOUT:
+                    alert('Geolocation Timeout');
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    alert('Geolocation Position unavailable');
+                    break;
+                case error.PERMISSION_DENIED:
+                    alert('Geolocation Permission denied');
+                    break;
+                default:
+                    alert('Geolocation returned an unknown error code: ' + error.code);
+            }
+        }
+
+        function stopWatching() {
+            if(watchID) geo.clearWatch(watchID);
+            watchID = null;
+
+        }
+
+        function startWatching() {
+            watchID = geo.watchPosition(initMap, geo_error, {
+                enableHighAccuracy: HIGHACCURACY,
+                maximumAge: MAXIMUM_AGE,
+                timeout: TIMEOUT
+            });
+        }
+
+        window.onload = function() {
+            if((geo = getGeoLocation())) {
+                startWatching();
+            } else {
+                alert('Geolocation not supported.')
+            }
+        }
